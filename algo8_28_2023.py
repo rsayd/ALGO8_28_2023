202141df4b582c2e1336b5c32e1d614840a0ec34
Ryan <rs77fl@gmail.com>
2023-08-28 23:38:18 -0700

    first commit

---
+++ algo8_28_2023.py
@@ -0,0 +1,42 @@
+"""
+  String: Reverse
+
+  Given a string,
+  return a new string that is the given string reversed
+"""
+
+const str1 = "creature";
+const expected1 = "erutaerc";
+
+const str2 = "dog";
+const expected2 = "god";
+
+const str3 = "hello";
+const expected3 = "olleh";
+
+const str4 = "";
+const expected4 = "";
+
+/**
+ * Reverses the given str.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str String to be reversed.
+ * @returns {string} The given str reversed.
+ */
+function reverseString(str) {}
+
+/*****************************************************************************/
+
+
+
+Answer:
+
+function reverseString(str) {
+    var empty = ""
+    for(i = str.length - 1; i >= 0; i--){
+        empty += str[i]
+    }
+    return empty
+}
+console.log(reverseString(str1))
